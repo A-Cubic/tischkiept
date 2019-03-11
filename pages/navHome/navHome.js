129f38592cde2bc3e2b4bc863d4e45e11fa83df7
Una_Bella <369152344@qq.com>
2019-03-11 23:24:05 +0800

    首页
    首页

---
+++ pages/navHome/navHome.js
@@ -0,0 +1,87 @@
+// pages/uploadBankcard/uploadBankcard.js
+//获取应用实例
+const app = getApp()
+Page({
+
+  data: {
+    allData:{
+      listHome:[
+        {
+          img:'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/A-test/bannerEuropeanAmerican.jpg'
+        },
+        {
+          img: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/A-test/bannerEuropeanAmerican.jpg'
+        }
+      ],
+      listShop:[]
+    },
+          
+    autoplay: true,//是否自动切换  
+    indicatorDots: true,//是否显示圆点  
+    interval: 5000,//自动切换间隔  
+    duration: 500, //滑动动画时长  
+    //indicatorColor: "rgba(255,255,255,0.24)",//滑动圆点颜色  
+    //indicatorActiveColor: "white", //当前圆点颜色 
+    indicatorColor: "blue",//滑动圆点颜色  
+    indicatorActiveColor: "white", //当前圆点颜色
+    current: 0, //当前所在页面的 index  
+    circular: true,  //是否采用衔接滑动  
+    imgheights: [],
+    //图片宽度 
+    imgwidth: 750,
+    //默认  
+    current: 0
+  },
+  onLoad: function () {
+    // this.imageLoad();
+    // this.getShopShow();
+  },
+  getShopShow:function(){
+    const that = this;
+    // 方法组名称为：User（代购用户），不是系统通用用户Users
+    app.Ajax(
+      'User',
+      'POST',
+      'GetShopShow',
+      { },
+      function (json) {
+        // console.log('json', json)
+        if (json.success) {
+          // that.imageLoad();
+          that.setData({
+            allData: json.data
+          })
+        }else{
+          app.Toast('', 'none', 3000, json.msg.code);
+          // wx.showToast({
+          //   title: json.msg.msg,
+          //   icon: 'none',
+          //   duration: 2500
+          // });
+        }
+      }
+    )
+  },
+  bindchange: function (e) {
+    // console.log(e.detail.current)
+    this.setData({ current: e.detail.current })
+  },
+
+  imageLoad: function (e) {//获取图片真实宽度  
+  console.log(e)
+    var imgwidth = e.detail.width,
+      imgheight = e.detail.height,
+      //宽高比  
+      ratio = imgwidth / imgheight;
+    // console.log(imgwidth, imgheight)
+    //计算的高度值  
+    var viewHeight = 750 / ratio;
+    var imgheight = viewHeight;
+    var imgheights = this.data.imgheights;
+    //把每一张图片的对应的高度记录到数组里  
+    imgheights[e.target.dataset.id] = imgheight;
+    this.setData({
+      imgheights: imgheights
+    })
+  },
+})
